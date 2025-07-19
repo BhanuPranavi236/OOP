4ba162d8cbeb35a64957bc6b71fd1bbac14d19a6
BhanuPranaviN <bpnerella@gmail.com>
2025-07-19 11:35:01 +0530

    Add files via upload

---
+++  firstbasicprograms/src/basicprograms/NonprimitiveDataTypes.java
@@ -0,0 +1,9 @@
+package basicprograms;
+
+public class NonprimitiveDataTypes {
+
+	public static void main(String[] args) {
+		String greeting = "Hello, World!";
+		System.out.println(greeting);
+	}
+}
