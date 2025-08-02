a1b5e5e86084d09b316fe5f4bcdfd582276d8ff6
BhanuPranavi236 <2420030236@klh.edu.in>
2025-08-02 10:59:16 +0530

    Add files via upload

---
+++ Skill/src/mypackage/skill3.java
@@ -0,0 +1,23 @@
+package mypackage;
+
+public class skill3 {
+	
+	     public static void main(String[] args) {
+	         int number = 7;
+	         if (number > 0) {
+	             System.out.println("Positive number");
+	         } else if (number < 0) {
+	             System.out.println("Negative number");
+	         } else {
+	             System.out.println("Zero");
+	         }
+
+	         System.out.println("Do-While loop:");
+	         int i = 1;
+	         do {
+	             System.out.println("i = " + i);
+	             i++;
+	         } while (i <= 3);
+	     }
+}
+
