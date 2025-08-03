b41ae4a6906b5917f5bc42a71132b10f204f39d3
BhanuPranavi236 <2420030236@klh.edu.in>
2025-08-03 11:57:58 +0530

    Add files via upload

---
+++ Practical/src/mypackage/Practical1.java
@@ -0,0 +1,28 @@
+package mypackage;
+
+public class Practical1 {
+	
+	    public static void main(String[] args) {
+	        int num = 10;
+	        double dbl = num; 
+	        int a = 5, b = 3;
+	        int total = a + b;
+	        boolean isGreater = a > b;
+
+	        if (a > b) {
+	            System.out.println("a is greater than b");
+	        }
+
+	        for (int i = 0; i < 5; i++) {
+	            System.out.println("i = " + i);
+	        }
+
+	        int[] arr = {1, 2, 3, 4, 5};
+	        for (int val : arr) {
+	            System.out.println(val);
+	        }
+	    }
+	}
+
+
+
